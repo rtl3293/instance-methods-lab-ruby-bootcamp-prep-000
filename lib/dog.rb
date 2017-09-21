64213c783d3cb17a22cea9be42f4f1d26ea50ec4
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-21 01:33:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
 class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
+  def initialize(name)
+    @this_dogs_name = name
 
   def name
     @this_dogs_name
+  end
+
+  def name=(new_name)
+    @this_dogs_name = dog_name
   end
 
   def bark
