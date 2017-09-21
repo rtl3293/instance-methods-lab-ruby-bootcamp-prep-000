1a280b0fd2c9cb4ea39a06c34b845b6a8062eec9
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-21 01:27:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,13 +10,13 @@ class Dog
   def bark
     puts "Woof!"
   end
-  
+
   def sit
     puts "The Dog is sitting"
   end
 end
 
-lassie = Dog.new()
+lassie = Dog.new
 
 lassie.name = "Lassie"
 print lassie.name
